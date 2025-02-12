9c2473f1758a892a7ba186484dc94cc839b38805
sistemasCOBAT <sistemas@cobatlaxcala.edu.mx>
2025-02-12 16:22:43 -0600

    primer proyecto

---
+++ assets/js/loader.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+    $('#onload').fadeOut();
+    $('body').removeClass('hidden');
+}
+
+// window.onload = () => {
+//     let loading = document.getElementById('onload');
+//     setTimeout(() => {
+//     loading.style.display = 'none';
+//     }, 15500)
+// }
