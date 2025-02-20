8eb6f540a9529e4c78d2f4f84c35ab9494e2caaf
sistemasCOBAT <sistemas@cobatlaxcala.edu.mx>
2025-02-20 12:50:57 -0600

    segundo commit

---
+++ assets/js/sweet_alert.js
@@ -0,0 +1,71 @@
+// Swal.fire({
+//     html: `<h1></h1>
+//     <p><strong></strong></p>
+//     <br>
+//     <a href="http://189.204.49.118/sistema.php" target="_blank">Clic Aquí</a>
+//     `,
+// 	imageUrl: '/COBAT/assets/images/img_actividades_cult_depor/Convocatoria Preinscripciones 2023.png',
+// });
+//<a href="/COBAT/assets/archivos/pdf/2023/TLAX_COBACH_23-24.pdf" target="_blank">Descargar PDF</a>
+
+// ORIGINAL
+Swal.fire({
+	// title:
+	text: '/assets/archivos/pdf/2023/Convocatoria.pdf',
+	html: 'INSCRIBETE',
+	// icon:
+	confirmButtonText: '<a href="https://forms.gle/YTUV8GLHQFNLJEE36" target="_blank">AQUÍ</a>',
+	// footer:
+	// width:
+	// padding:
+	// background:
+	grow: 'fullscreen',
+	// backdrop:
+	timer: '50000',
+	timerProgressBar: 'true',
+	// toast:
+	// position:
+	// allowOutsideClick: 'false',
+	// allowEscapeKey: 'false',
+	// allowEnterKey: 'false',
+	// stopKeydownPropagation: 'false',
+
+	// input:
+	// inputPlaceholder:
+	// inputValue:
+	// inputOptions:
+	
+	//  customClass:
+	// 	container:
+	// 	popup:
+	// 	header:
+	// 	title:
+	// 	closeButton:
+	// 	icon:
+	// 	image:
+	// 	content:
+	// 	input:
+	// 	actions:
+	// 	confirmButton:
+	// 	cancelButton:
+	// 	footer:	
+
+	// showConfirmButton:
+	// confirmButtonColor:
+	// confirmButtonAriaLabel:
+
+	// showCancelButton:
+	// cancelButtonText:
+	// cancelButtonColor:
+	// cancelButtonAriaLabel:
+	
+	// buttonsStyling:
+	// showCloseButton:
+	// closeButtonAriaLabel:
+
+
+	imageUrl: 'assets/images/slider/Convocatoria Talentos COBAT 2024 v3.jpg'
+	// imageWidth:
+	// imageHeight:
+	// imageAlt:
+});
